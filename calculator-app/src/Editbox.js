b4e5f1685259d646e6b4c6ef0ccdfdf895dc3db7
Mutheeswari <mutheeswari27@gmail.com>
2022-07-09 00:00:05 +0530

    App.css index.js EditBox.js files are changed

---
+++ calculator-app/src/Editbox.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function Editbox() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <input type="text" id="edittxt" name="edittxt" class="set-width" value="0123456789" dir="rtl">
+        {/*
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+
+          Welcome to Versatile product
+          
+        </a>
+        <a className="App-link1"></a>
+          <p class="solid">calculator</p>
+  */}
+      </input>
+      </header>
+    </div>
+  );
+}
+
+export default Editbox;
