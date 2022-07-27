b5b5a408378cd0cc81dbabd226e950d9cac5d016
chris.reejoy90@gmail.com <chris.reejoy90@gmail.com>
2022-07-27 11:15:43 +0530

    editbox and buttonclear file modified

---
+++ calculator-app/src/ButtonClear.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function ButtonClear() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+      <button type="button" class="Buttonclear button">Clear</button > 
+      <button type="button" class="Buttonclear button">Del</button > 
+      </div>
+        {/*
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Welcome to my new project
+        </a>
+  */}
+      </header>
+    </div>
+  );
+}
+
+export default ButtonClear;
