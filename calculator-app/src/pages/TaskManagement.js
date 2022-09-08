fcd75fcec73b6139da7c2eb8e336db4abd5b62c2
Mutheeswari <mutheeswari27@gmail.com>
2022-09-08 13:21:05 +0530

    className modified intead of class

--- calculator-app/src/pages/TaskManagement.js
+++ calculator-app/src/pages/TaskManagement.js
@@ -3,16 +3,16 @@ import "./App.css";
 const TaskManagement = () => {
   return (
     <>
-      <div class="container">
-        <div class="jumbotron">
+      <div className="container">
+        <div className="jumbotron">
           <h1>My TODO List</h1>
-          <div class="form-group">
-            <div class="row">
-              <div class="col-sm-1">
+          <div clclassNameass="form-group">
+            <div className="row">
+              <div className="col-sm-1">
                 <label for="usr">Enter Task : </label>
               </div>
-              <div class="col-sm-9"><input type="text" class="form-control" id="usr" /></div>
-              <div class="col-sm-2"><button type="button" class="btn btn-info">X</button></div>
+              <div className="col-sm-9"><input type="text" className="form-control" id="usr" /></div>
+              <div className="col-sm-2"><button type="button" className ="btn btn-info">X</button></div>
             </div>
           </div>
         </div>
